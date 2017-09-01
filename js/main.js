20e2e810a8f7a7ba08577aa9199fcd0bb04c1a28
Captain-Oski <glogowski.clement@gmail.com>
2017-09-01 13:48:30 -0400

    new dynamic sidebar

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
-var map = L.map('map').setView([45.5314, -73.6750], 11);
+var map = L.map('map',{ zoomControl: false }).setView([45.5314, -73.6750], 11);
 
+new L.Control.Zoom({ position: 'topright' }).addTo(map);
 
 var baseLayer1 = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png', {
   maxZoom: 18,

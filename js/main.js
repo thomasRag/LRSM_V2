c73be04b63acc5c824ebcec76fc3a83003286f85
Captain-Oski <glogowski.clement@gmail.com>
2017-09-06 13:20:13 -0400

    no more ajax request

--- js/main.js
+++ js/main.js
@@ -7,3 +7,120 @@ var baseLayer1 = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net
   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>'
  })
  .addTo(map);
+
+console.log(featuresCollection)
+
+var group = L.layerGroup();
+
+
+
+setColor = {
+ radius: 8,
+ fillColor: get_random_color(),
+ color: '#ffffff',
+ weight: 1,
+ opacity: 1,
+ fillOpacity: 0.5
+
+};
+
+function get_random_color() {
+  var letters = '0123456789ABCDEF'.split('');
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+   color += letters[Math.round(Math.random() * 15.66)];
+  }
+  return color;
+ }
+
+
+ function removeLayers(i) {
+  map.removeLayer(i)
+ };
+
+/********** GET FILTERED MARKERS OVER THE SPECIFIC STORY ID ******/
+
+function getFilterMarkers (j){
+
+   filterMarkers = L.geoJSON(featuresCollection,{
+
+     filter: function(feature, layer) {
+      return feature.properties.story.id === j},
+
+      pointToLayer: function(feature, latlng) {
+       return L.circleMarker(latlng, setColor) //, style(feature)); //,styled(feature));
+      },
+
+      onEachFeature: function(feature, layer) {
+       //layer.bindPopup('<h1>'+marker.properties.title+'</h1><p>'+marker.properties.auteurs+'</p>'+'</h1><p>'+marker.properties.id+'</p><a href=www.google.com>Explorer le récit</a>');
+       console.log(feature.properties.media)
+       layer.bindPopup('<div><a href="#" class="speciallink"><h2>'
+        +feature.properties.title +'<a data-flickr-embed="true"  href="https://www.flickr.com/photos/thexplorer/1590316457/" title="Untitled"><img src="https://farm3.staticflickr.com/2214/1590316457_6e1aa70c12_q.jpg" width="150" height="150" alt="Untitled"></a><script async src="//embedr.flickr.com/assets/client-code.js" charset="utf-8"></script>')}
+
+
+
+
+    }).addTo(map)
+
+
+    removeLayers(group)
+    map.fitBounds(filterMarkers.getBounds())
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ function getMainMarkers(){
+
+ 	 introMarker = L.geoJSON(featuresCollection,{
+
+     filter: function(feature, layer) {
+      return feature.properties.main === true},
+
+      pointToLayer: function(feature, latlng) {
+       return L.circleMarker(latlng, setColor) //, style(feature)); //,styled(feature));
+      },
+
+      onEachFeature: function(feature, layer) {
+       //layer.bindPopup('<h1>'+marker.properties.title+'</h1><p>'+marker.properties.auteurs+'</p>'+'</h1><p>'+marker.properties.id+'</p><a href=www.google.com>Explorer le récit</a>');
+
+       layer.bindPopup($('<div><a href="#" class="speciallink"><h2>'
+        +feature.properties.title + '</h2></a><iframe id="player1" src="https://player.vimeo.com/video/69426498?title=0&amp;byline=0&amp;portrait=0&amp;autoplay=0&amp;api=1&amp;player_id=player1" width="100%" height="80%" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe><div>').click(
+
+
+        function() {
+
+          getFilterMarkers(feature.properties.story.id),
+          removeLayers(getFilterMarkers),
+          $("#myModal").modal()
+
+       })[0]);
+
+
+      }
+
+    })
+
+
+
+
+ group.addLayer(introMarker).addTo(map)
+ map.fitBounds(introMarker.getBounds())
+
+
+ }
+
+ getMainMarkers()

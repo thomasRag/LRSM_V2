4b253fe370c7ef25ad80fd5b33df20b64f84ecf6
Captain-Oski <glogowski.clement@gmail.com>
2017-11-22 16:22:39 -0500

    modal filtering on close

--- js/main.js
+++ js/main.js
@@ -58,6 +58,8 @@ var authors = $("#authorSearchBox").val();
 function getFilterMarkers (i){
   filteredGroup.clearLayers();
      filterMarkers = L.geoJSON(featuresCollection,{
+
+
          filter:          function(feature, layer) {
           for (var j = 0 ; j < i ; j++){
            if (feature.properties.story['id'] == i){
@@ -75,44 +77,42 @@ function getFilterMarkers (i){
            layer.bindPopup('<div><a href="#" class="speciallink"><h2>'
             +feature.properties.story[i] +'</h2></div>')
 
-
-
           }
 
     }).addTo(map)
 
 
-    group.clearLayers();
-    filteredGroup.addLayer(filterMarkers).addTo(map)
-   //map.fitBounds(filterMarkers.getBounds())
+  group.clearLayers();
+  filteredGroup.addLayer(filterMarkers).addTo(map)
+  map.fitBounds(filterMarkers.getBounds().pad(Math.sqrt(2) / 2))
+
 
 }
-
 
 
 function openModal(){
   $("#myModal").modal('show')
   map.closePopup();
-}
+  }
 
- function getMainMarkers(){
+document.getElementById("modalClose").onclick = function () { getMainMarkers() };
+
+ function getMainMarkers(id){
 
  	 introMarker = L.geoJSON(featuresCollection,{
 
-    /* filter: masterFilter,*/
+    /* filter: function(feature, layer) {
+       if(feature.properties.story['id'] === storyID)
+     },*/
 
       pointToLayer: function(feature, latlng) {
          label = String(feature.properties.story.id)
        return L.circleMarker(latlng, setColor).bindTooltip(label, {permanent: true, opacity: 0.7}).openTooltip();
 		},
-  /*    filter: function(feature) {
-        if (feature.properties.main == true)
-        return true
-      },*/
 
-      
-      onEachFeature: onEachFeature 
-      
+
+      onEachFeature: onEachFeature
+
     })
 
 
@@ -137,39 +137,12 @@ function onEachFeature(feature, layer) {
     customPopup = customPopup + '<div id="mediaIcon" class="">'
     customPopup = customPopup + '<img id="mediaIconImg" class="rounded-circle" src="img/Icon_Multimedia.png"  width="40px"/>'
     customPopup = customPopup + '</div>'
-    customPopup = customPopup + '<a href="javascript:openModal();"><img id="popUpImg" class="card-img-top" src="img/test.jpg" alt="Card image cap">'
+    customPopup = customPopup + '<a href="javascript:getFilterMarkers('+feature.properties.story['id']+'),openModal()"><img id="popUpImg" class="card-img-top" src="img/test.jpg" alt="Card image cap">'
     customPopup = customPopup + '<div id="popUpFooter" class="card-footer pl-1 mt-1 pt-1 mb-0 pb-0">'
     customPopup = customPopup + '<h5 id="popUpTitle" class="mt-0"> Titre du récit </h5>'
     customPopup = customPopup + '<p id="popUpAuthor" class="mt-1 pl-1 mb-0 pb-1">Nom de l\'auteur</p>'
     customPopup = customPopup +  '</div></a>'
     customPopup = customPopup +  '</div></div>'
-
-
-  /*  layer.bindPopup($('<div><a href="#" class="speciallink"><h2>'
-              +feature.properties.title + '</h2></a><iframe id="player1" src="https://player.vimeo.com/video/69426498?title=0&amp;byline=0&amp;portrait=0&amp;autoplay=0&amp;api=1&amp;player_id=player1" width="100%" height="80%" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe><div>').click(
-                function() {
-                  jQuery("#myModal").modal('show')
-                  })[0]);
-                  }
-                })*/
-
-
-
-			var lg = group[feature.properties.story['id']];
-
-			    if (lg === undefined) {
-			        lg = new L.layerGroup();
-			        //add the layer to the map
-			        lg.addTo(map);
-			        //store layer
-			        group[feature.properties.story['id']] = lg;
-			    }
-
-			    //add the feature to the layer
-			    lg.addLayer(layer);      
-		
-
-
 
 
     // specify popup options
@@ -183,58 +156,14 @@ function onEachFeature(feature, layer) {
 /************** fin popup *****************/
 
 /************** debut modal populate *****************/
-       /*var container = $('<div id="container'+feature.properties.story['id']+'"><row chapter><div class="col-md-10"> <span id="elementNumber"> <h4>Élément #'+feature.properties.story['id']+'   </h4> <span>       </div><div class="col-md-10"><span> <h5> Emplacement | Date </h5>  </span></div></div><div class="row"></div><div class="row"><div class="col-md-10 mx-auto d-inline-block mt-2 pt-2 text-justify"><span> <p>  </p> </span> </div> </div><hr></div>',{
-       });*/
+
 
           var chapter = $('<p></p><row chapter><div class="col-md-10"> <span id="elementNumber"> <h4>Élément #'+feature.properties.story['id']+'   </h4> <span>       </div><div class="col-md-10"><span> <h5> Emplacement | Date </h5>  </span></div></div><div class="row"></div><div class="row"><div class="col-md-10 mx-auto d-inline-block mt-2 pt-2 text-justify"><span> <p>  </p> </span> </div> </div><hr></div>', {
                 text: feature.properties.story['id'],
                 class: 'chapter-header'
               });
 
-  /*       var container = $('<div></div>', {
-             id: 'container' + feature.properties.story['id'],
-
-           });
-
-           var description = $('<div></div>', {
-             text: feature.properties.story['text'],
-             class: 'description'
-           });
-
-           container.append(chapter).append(description)
-           $('#modalBodyContent').append(container)
-
-           var imageContainerMargin = 270;  // Margin + padding
-
-           // This watches for the scrollable container
-           var scrollPosition = 0;
-           $('#modalBodyContent').scroll(function() {
-             scrollPosition = $(this).scrollTop();
-           });
-
-           var i;
-           var areaTop = -100;
-           var areaBottom = 0;
-
-           // Calculating total height of blocks above active
-           for (i = 1; i < feature.properties.story['id']; i++) {
-
-             areaTop += $('#container' + feature.properties['id']).height() + imageContainerMargin;
-           }
-
-           areaBottom = areaTop + $('#container' + feature.properties.story['id']).height();
-
-
-          $('#modalBodyContent').scroll(function() {
-             if ($(this).scrollTop() >= areaTop && $(this).scrollTop() < areaBottom) {
-          $('.test').removeClass("inFocus").addClass("outFocus");
-          $('#container' + feature.properties.story['id']).addClass("inFocus").removeClass("outFocus");
-          map.flyTo([feature.geometry.coordinates[1], feature.geometry.coordinates[0] ], 18);
-             }
-           });*/
-
-
-           /***********************************************************************/
+/***********************************************************************/
 
            var container = $('<div></div>', {
                        id: 'container' + feature.properties['id'],
@@ -274,45 +203,12 @@ function onEachFeature(feature, layer) {
                      });
 
 /************************************************************************/
-
-
-           // Make markers clickable
-           layer.on('click', function() {
-
-             $("#modalBodyContent").animate({scrollTop: areaTop + "px"});
-           	//getFilterMarkers(feature.properties.story['id'])
-           		
-           	showLayer(feature.properties.story['id'])
-           	
+// Make markers clickable
+  layer.on('click', function() {
+     $("#modalBodyContent").animate({scrollTop: areaTop + "px"});
            });
         })(layer, feature.properties)
 /************** fin modal populate *****************/
 
 
-
-/*
-introMarker.on("click", groupClick);
- 
-
- 	function groupClick(event) {
-  console.log(event.properties);
-}*/
-
 }
-
-function showLayer(id) {
-
-	for (var i = 0; i < 1000; i++) {
-	hideLayer(i)};
-
-	//var lg = group[id];
-    //map.addLayer(lg);   
-}
-function hideLayer(id) {
-    
-    var lg = group[id];
-    map.removeLayer(lg);   
-}
- /******************************/
-
-
